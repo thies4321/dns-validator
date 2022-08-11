118e3d25242ad4062cdfde7cd77f3ad7d7d53bb3
TransIP <transip@fedora.fritz.box>
2022-08-12 00:14:31 +0200

    more records added

--- src/Entity/ResourceRecord.php
+++ src/Entity/ResourceRecord.php
@@ -9,12 +9,15 @@ use DnsValidator\Exception\InvalidResourceRecordType;
 
 final class ResourceRecord
 {
+    private string $name;
+    private int $ttl;
     private ResourceRecordType $type;
+    private string $content;
 
     /**
      * @throws InvalidResourceRecordType
      */
-    public function __construct(string $type)
+    public function __construct(string $name, int $ttl, string $type, string $content)
     {
         $resourceRecordType = ResourceRecordType::tryFrom($type);
 
@@ -22,8 +25,34 @@ final class ResourceRecord
             throw InvalidResourceRecordType::forType($type);
         }
 
+        $this->name = $name;
+        $this->ttl = $ttl;
         $this->type = $resourceRecordType;
+        $this->content = $content;
     }
 
+    public function getName(): string
+    {
+        return $this->name;
+    }
 
+    public function getTtl(): int
+    {
+        return $this->ttl;
+    }
+
+    public function getType(): string
+    {
+        return $this->type->value;
+    }
+
+    public function getDefiningRfc(): string
+    {
+        return $this->type->getReference();
+    }
+
+    public function getContent(): string
+    {
+        return $this->content;
+    }
 }
--- src/Enum/ResourceRecordType.php
+++ src/Enum/ResourceRecordType.php
@@ -26,6 +26,33 @@ enum ResourceRecordType: string
     case CERT = 'CERT'; // 37
     case DNAME = 'DNAME'; // 39
     case APL = 'APL'; // 42
+    case DS = 'DS'; // 43
+    case SSHFP = 'SSHFP'; // 44
+    case IPSECKEY = 'IPSECKEY'; // 45
+    case RRSIG = 'RRSIG'; // 46
+    case NSEC = 'NSEC'; // 47
+    case DNSKEY = 'DNSKEY'; // 48
+    case DHCID = 'DHCID'; // 49
+    case NSEC3 = 'NSEC3'; // 50
+    case NSEC3PARAM = 'NSEC3PARAM'; // 51
+    case TLSA = 'TLSA'; // 52
+    case SMIMEA = 'SMIMEA'; // 53
+    case HIP = 'HIP'; // 55
+    case CDS = 'CDS'; // 59
+    case CDNSKEY = 'CDNSKEY'; // 60
+    case OPENPGPKEY = 'OPENPGPKEY'; // 61
+    case CSYNC = 'CSYNC'; // 62
+    case ZONEMD = 'ZONEMD'; // 63
+    case SVCB = 'SVCB'; // 64
+    case HTTPS = 'HTTPS'; // 65
+    case EUI48 = 'EUI48'; // 108
+    case EUI64 = 'EUI64'; // 109
+    case TKEY = 'TKEY'; // 249
+    case TSIG = 'TSIG'; // 250
+    case URI = 'URI'; // 256
+    case CAA = 'CAA'; // 257
+    case TA = 'TA'; // 32768
+    case DLV = 'DLV'; // 32769
 
     public function getReference(): string
     {
@@ -45,6 +72,26 @@ enum ResourceRecordType: string
             self::CERT => RequestForComment::RFC_4398->value,
             self::DNAME => RequestForComment::RFC_6672->value,
             self::APL => RequestForComment::RFC_3123->value,
+            self::DS, self::RRSIG, self::NSEC, self::DNSKEY => RequestForComment::RFC_4034->value,
+            self::SSHFP => RequestForComment::RFC_4255->value,
+            self::IPSECKEY => RequestForComment::RFC_4025->value,
+            self::DHCID => RequestForComment::RFC_4701->value,
+            self::NSEC3, self::NSEC3PARAM => RequestForComment::RFC_5155->value,
+            self::TLSA => RequestForComment::RFC_6698->value,
+            self::SMIMEA => RequestForComment::RFC_8162->value,
+            self::HIP => RequestForComment::RFC_8005->value,
+            self::CDS, self::CDNSKEY => RequestForComment::RFC_7344->value,
+            self::OPENPGPKEY => RequestForComment::RFC_7929->value,
+            self::CSYNC => RequestForComment::RFC_7477->value,
+            self::ZONEMD => RequestForComment::RFC_8976->value,
+            self::SVCB, self::HTTPS => RequestForComment::RFC_IN_DRAFT->value,
+            self::EUI48, self::EUI64 => RequestForComment::RFC_7043->value,
+            self::TKEY => RequestForComment::RFC_2930->value,
+            self::TSIG => RequestForComment::RFC_2845->value,
+            self::URI => RequestForComment::RFC_7553->value,
+            self::CAA => RequestForComment::RFC_6844->value,
+            self::TA => RequestForComment::RFC_UNKNOWN->value,
+            self::DLV => RequestForComment::RFC_4431->value,
         };
     }
 }
